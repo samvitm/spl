b9910010ac50f90f8040f4ad3aaa0866408476f0
Samvit <samvit.1@gmail.com>
2012-04-11 20:51:33 +0530

    raised syntax error,  added support for keyword float

--- lexer.py
+++ lexer.py
@@ -1,5 +1,5 @@
 import re
-t = re.compile(r'''(?P<TK_Semicolon>;)|(?P<TK_Comma>,)|(?P<TK_LFBK>\()|(?P<TK_RFBK>\))|(?P<TK_LFBR>\{)|(?P<TK_RFBR>\})|(?P<TK_ASSIGN>\=)|(?P<TK_PLUS>\+)|(?P<TK_STAR>\*)|(?P<TK_FLOAT>\d+\.{1}\d+)|(?P<TK_INTLIT>\d+)|(?P<TK_INT>int)|(?P<TK_STRING>string)|(?P<TK_ID>[a-zA-Z][a-zA-Z0-9]*)|(?P<TK_STRLIT>"(.*?(\\")*.*?)+")''')
+t = re.compile(r'''(?P<TK_Semicolon>;)|(?P<TK_Comma>,)|(?P<TK_LFBK>\()|(?P<TK_RFBK>\))|(?P<TK_LFBR>\{)|(?P<TK_RFBR>\})|(?P<TK_ASSIGN>\=)|(?P<TK_PLUS>\+)|(?P<TK_STAR>\*)|(?P<TK_REAL_LIT>\d+\.{1}\d+)|(?P<TK_INTLIT>\d+)|(?P<TK_INT>int)|(?P<TK_STRING>string)|(?P<TK_FLOAT>float)|(?P<TK_ID>[a-zA-Z][a-zA-Z0-9]*)|(?P<TK_STRLIT>"(.*?(\\")*.*?)+")''')
 s,ln,st = open("sample.txt").read().rstrip(),0,[]
 def nextToken():
   global s,ln,st
@@ -11,8 +11,8 @@ def nextToken():
     s,st = s[len(match.group()):],st+[(len(st)+1,str(match.group()),match.lastgroup,)]
     return match.lastgroup
   else:
-    print 'Unidentified token : "',s[0],'" at line number ',ln+1
-    exit()
+    raise SyntaxError('Unknown symbol %r at line %r' %(s[0],ln))
+
 #testing
 while len(s):
   print nextToken()
