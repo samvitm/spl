48fe36d420ffee20ca24977fbc85d28f302baa84
U-Ednha-PC\Ednha <Ednha@Ednha-PC.(none)>
2012-04-22 07:43:10 +0530

    Parser done for the given grammar, debugging lines still there, need to maintain symbol table and scope.

--- lexer.py
+++ lexer.py
@@ -1,21 +1,29 @@
 import re
-t = re.compile(r'''(?P<TK_Semicolon>;)|(?P<TK_Comma>,)|(?P<TK_LFBK>\()|(?P<TK_RFBK>\))|(?P<TK_LFBR>\{)|(?P<TK_RFBR>\})|(?P<TK_ASSIGN>\=)|(?P<TK_PLUS>\+)|(?P<TK_STAR>\*)|(?P<TK_REAL_LIT>\d+\.{1}\d+)|(?P<TK_INTLIT>\d+)|(?P<TK_INT>int)|(?P<TK_STRING>string)|(?P<TK_FLOAT>float)|(?P<TK_ID>[a-zA-Z][a-zA-Z0-9]*)|(?P<TK_STRLIT>"(.*?(\\")*.*?)+")''')
+t = re.compile(r'''(?P<TK_Semicolon>;)|(?P<TK_Comma>,)|(?P<TK_LFBK>\()|(?P<TK_RTBK>\))|(?P<TK_LFBR>\{)|(?P<TK_RTBR>\})|(?P<TK_ASSIGN>\=)|(?P<TK_PLUS>\+)|(?P<TK_STAR>\*)|(?P<TK_REAL_LIT>\d+\.{1}\d+)|(?P<TK_INTLIT>\d+)|(?P<TK_INT>int)|(?P<TK_STRING>string)|(?P<TK_FLOAT>float)|(?P<TK_ID>[a-zA-Z][a-zA-Z0-9]*)|(?P<TK_STRLIT>"(.*?(\\")*.*?)+")''')
 s,ln,st = open("sample.txt").read().rstrip(),0,[]
-def nextToken():
+def getnextToken():
   global s,ln,st
+  if not len(s) : return 0
   while s[0] in ['\n','\r','\r\n',' ','\t']:
     if s[0] not in[' ','\t']: ln+=1
     s = s[1:]
   match = t.match(s)
   if match:
-    s,st = s[len(match.group()):],st+[(len(st)+1,str(match.group()),match.lastgroup,)]
+    s,st = s[len(match.group()):],st+[(len(st)+1,str(match.group()),match.lastgroup,ln+1,)]
     return match.lastgroup
   else:
-    raise SyntaxError('Unknown symbol %r at line %r' %(s[0],ln))
+    raise SyntaxError('Unknown symbol %r at line %r' %(s[0],ln+1))
+
 
 #testing
-while len(s):
-  print nextToken()
-print 'Checking symbol table ... '
+def build_tokens():
+  l = []
+  while len(s):
+    l.append(getnextToken())
+  return l
+
+tokens = build_tokens()
+
+print 'Checking token table ... '
 for e in st:
   print e
---
+++ parser.py
@@ -0,0 +1,103 @@
+from lexer import tokens,st
+import re
+
+#print tokens
+count = -1
+def nextToken():
+  global count
+  count+=1
+  try:
+    token = tokens[count]
+  except :
+    return '$'
+  return token
+
+parse_table = {
+    "<program>" : {"TK_INT" : 1 , "TK_FLOAT" :1 , "TK_STRING" : 1 ,"TK_Comma" : 1},
+    "<functionbody>" : { "TK_LFBR" : 2 },
+    "<functions>" : {"TK_INT" : 3 , "TK_FLOAT" : 3, "TK_STRING" : 3 , "TK_Comma" : 4},
+    "<function>" : {"TK_INT" : 5,"TK_FLOAT" : 5 , "TK_STRING" : 5},
+    "<functionsignature>" : {"TK_INT" : 6,"TK_FLOAT" : 6,"TK_STRING" :6},
+    "<type>" : {"TK_INT":7,"TK_FLOAT" : 8,"TK_STRING" : 9},
+    "<params>" : { "TK_INT" : 10,"TK_FLOAT" : 10,"TK_STRING":10,"TK_RTBK":11},
+    "<declarations>" : {"TK_INT" : 12 , "TK_FLOAT" : 12 ,"TK_STRING" : 12,"TK_ID" : 13,"TK_Comma" : 13},
+    "T" : {"TK_ID" : 14 ,"TK_INTLIT" : 14,"TK_REAL_LIT" : 14,"TK_STRLIT" : 14},
+    "T!" : {"TK_STAR" : 15,"TK_PLUS" : 16,"TK_Semicolon" : 16},
+    "<args>" : {"TK_ID" : 17 ,"TK_INTLIT" : 17,"TK_REAL_LIT" : 17,"TK_STRLIT" : 17,"TK_RTBK" :18},
+    "<statements>" : {"TK_ID" : 19,"TK_STAR":20},
+    "<statements!>" : {"TK_ID":21,"TK_INTLIT":22,"TK_REAL_LIT":23,"TK_STRLIT":24},
+    "<more>" : {"TK_LFBK":25,"TK_PLUS":26,"TK_Semicolon":26,"TK_STAR":27},
+    "<expr>" : {"TK_ID" : 28 ,"TK_INTLIT" : 28,"TK_REAL_LIT" : 28,"TK_STRLIT" : 28},
+    "<expr!>" : {"TK_PLUS" : 29,"TK_Semicolon":30},
+    "F" : {"TK_ID" : 31 ,"TK_INTLIT" : 32,"TK_REAL_LIT" : 33,"TK_STRLIT" : 34},
+}
+
+stack = ['$',"<program>"]
+
+def get_grammar():
+  f = open('grammar.txt')
+  g = [' ',] #compensate for the 0'th grammar rule
+  for line in f:
+    g.append(line.strip())
+  return g
+
+def get_terminals():
+  f = open('terminals.txt')
+  t = []
+  for line in f:
+    t.append(str(line.strip()))
+  return t
+
+terminals = get_terminals()
+grammar = get_grammar()
+
+def get_rhs(s):
+  m = s.split('->')[-1]
+  return m.split()
+
+
+def parse():
+  global stack
+  a = nextToken()
+  x = stack[-1]
+  while x != '$':
+    print stack,x,a
+    if x == a:
+      stack.pop()
+      print stack
+      a = nextToken()
+      print 'Next token to parse : ',a
+    elif x in terminals:
+      print 'Error - This is a terminal',x,st[count],a
+      exit()
+    elif  a not in parse_table[x] :
+      print a,x,st[count]
+      print 'Error - No matching entry in parse table'
+      exit()
+    else:
+      rule = str(grammar[parse_table[x][a]])
+      print 'Match found!'
+      print a,x,st[count]
+      print 'Applying grammar rule  : ',rule
+      #print stack
+      stack.pop()
+      #print stack
+      stack = stack + get_rhs(rule)[::-1]
+      print stack
+      print '-----END----'
+    x = stack[-1]
+    print 'Stack top now : ',x
+
+    #print a,'||-- x :',x,'||--stack :',stack
+
+
+parse()
+print 'Parse was successful!'
+
+
+
+
+
+
+
+
